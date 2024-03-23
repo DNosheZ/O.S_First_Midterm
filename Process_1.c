0740320637f630397fa7e4d0ca18bc548c562be6
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-03-22 21:02:05 -0500

    Update Process_1.c

--- Process_1.c
+++ Process_1.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]){
 	ptr=mmap(0,SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	
 	//desde aqui se crea el semaforo para escribir la direccion del comando a procesar
-	const char sh[]="/SOMH";
+	const char sh[]="/SOMH";//cuantos semaforos requerira este proceso
 	sem_t *semola=sem_open(sh, O_CREAT, 0666,0);
 	if (semh==SEM_FAILED){
 	  	perror("Error en sem_open\n");
@@ -31,21 +31,36 @@ int main(int argc, char *argv[]){
 
 	char ruta[1024];
 	FILE *fp;
-	if(argc<=2){/*verifico el paso de parametros*/
-	  sem_wait(semola);
-
-	  sem_post(semola);  
+	if(argc==2){/*verifico el paso de parametros*/
+	 //el semaforo funcionara desde la revision de la ruta
+			   //de acceso, o solo para guardar la ruta
+			   //en la tuberia sin nombre
+	  const char *ruta= argv[1];//ruta de acceso del comando a ejecutar
+	  printf("Uso: p1 %s\n", ruta);
+	    //aqui se ingresa la ruta de acceso en la tuberia
+	    //empleando el pid del proceso padre
+	    //verifica la existencia de la ruta de acceso
+	  int pvRuta=open(ruta,O_RDONLY);
+	   if(pvRuta<0){
+		 return(-1);
+	   }else{
+		if(fork()==0){//Proceso hijo==Proceso_2
+		  //variable para la lectura de la tuberia en formato string
+		  char lectura[50];
+		  read(fildes[0], lectura, strlen(ruta));
+		  //aqui va la verificacion de la ruta de acceso
+		  //crear segunda tuberia sin nombre para comunicar de proceso 2
+		  //a proceso 1	    
+     	   	}else{//Proceso padre==Proceso_1
+		  write(fildes[1], ruta, strlen(ruta)); 
+	   	} 
 	//probablemente deba crear un semaforo para proteger y dar tiempo
 	//al guardado de la direccion en el espacio de memoria
-	}else{
-	sem_wait(semola);
-	ptr+=strlen();//en que formato pondremos lo direccion de archivo del comando ingresado
-	sem_post(semola);
-	
+	}else{//mensaje default en caso de que no se asigne un parametro
+              //de ejecucion
+	  printf("Uso: p1 /ruta/al/ejecutable");
 	}
 	/*este proceso agrega a un espacio de memoria compartido 
-	  la direccion de memoria del proceso que se paso como parametro
-	*/
-	sem_close(semola);
+	  la direccion de memoria del proceso que se paso como parametro*/
 	return 0;
 }
