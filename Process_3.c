2eb77e7030220858fdbdb061f6b1ac759261eac4
smierl <145398729+smierl@users.noreply.github.com>
2024-03-28 14:43:30 -0500

    Update Process_3.c

---
+++ Process_3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define MC "/myMemoryObj"
+#define MC_SIZE 1024
+
+int main() {
+    int memoria_compartida = shm_open(MC, O_RDWR, 0);
+    if (memoria_compartida == -1) {
+        perror("Error al abrir memoria compartida");
+        exit(EXIT_FAILURE);
+    }
+
+    char *ptr;
+    ptr = mmap(0, MC_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memoria_compartida, 0);
+    if (ptr == MAP_FAILED) {
+        perror("Error al mapear memoria compartida");
+        exit(EXIT_FAILURE);
+    }
+  
+    if (dup2(memoria_compartida, STDOUT_FILENO) == -1) {
+        perror("Error al redirigir salida estándar");
+        exit(EXIT_FAILURE);
+    }
+
+    char comando[MC_SIZE];
+    strcpy(comando, ptr);
+  
+    memset(ptr, 0, MC_SIZE);
+  
+    if (execlp(comando, comando, NULL) == -1) {
+        perror("Error al ejecutar el comando");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
